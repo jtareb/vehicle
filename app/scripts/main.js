b3b7c9454362cbae433569b4a9fbb80bf5b1b840
John Albritton <jtareb1@att.net>
2015-04-28 23:39:34 -0400

    fix

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 ;(function (){
   
-  angular.module('Banking', ['ngRoute'])
+  angular.module('Vehicle', ['ngRoute'])
 
   .constant('PARSE', {
     URL: 'https://api.parse.com/1/',
@@ -18,18 +18,6 @@
 
     $routeProvider
 
-       // Login Page
-          .when('/login', {
-            templateUrl: 'scripts/Users/user.login.tpl.html',
-            controller: 'UserCtrl'
-          })
-
-        // Register Page 
-          .when('/register', {
-            templateUrl: 'scripts/Users/user.register.tpl.html',
-            controller: 'UserCtrl'
-          })
-
           .when('/', {
             templateUrl: 'scripts/vehicle/list.tpl.html',
             controller: 'VehicleCtrl'
@@ -38,7 +26,6 @@
             templateUrl: 'scripts/vehicle/add.tpl.html',
             controller: 'VehicleCtrl'
           })
-
           .when('/edit', {
             templateUrl: 'scripts/vehicle/editVehicle.tpl.html',
             controller: 'VehicleCtrl'
