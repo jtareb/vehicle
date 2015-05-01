99afac9530d178c47a3c5b7bc2814889ef10658a
John Albritton <jtareb1@att.net>
2015-05-01 12:31:36 -0400

    fix

--- app/scripts/vehicle/vehicle.controller.js
+++ app/scripts/vehicle/vehicle.controller.js
@@ -1,25 +1,76 @@
-;(function (){
+(function() {
 
   angular.module('Vehicle')
+  .controller('VehicleCtrl', ['$scope', 'ShowsFactory', '$rootScope', '$cacheFactory',
 
-    .controller('VehicleCtrl', ['$scope', 'PostsFactory', '$rootScope', '$location',
+    function($scope, ShowsFactory, $rootScope, $cacheFactory){
 
-      function ($scope, PostsFactory, $rootScope, $location) {
+      var cache = $cacheFactory.get('http');
 
-         VehicleFactory.get().success( function (data) {
+      $scope.allVehicles = [];
+
+      ShowsFactory.get().success( function(data){
           $scope.allVehicles = data.results;
-         
-        }),
-
-        $scope.addVehicle = function (p) {
-          VehicleFactory.add(p);
-
-        }, 
-    
-        $rootScope.$on('vehicle:added', function () {
-          $location.path('/');
         });
-      }
-    ]);
+
+
+
+      $scope.addVehicle = function(w){
+
+
+        ShowsFactory.add(w);
+        console.log(w);
+
+
+      };
+
+      $rootScope.$on('Vehicle:add', function(){
+      });
+
+    }
+
+  ]);
 
 }());
+
+
+
+
+
+
+
+
+
+
+
+//;(function (){
+
+ // 'use strict'
+
+ // angular.module('Vehicle')
+
+ //   .controller('VehicleCtrl', ['$scope', 'VehicleFactory', '$rootScope', '$location', 
+
+ //     function ($scope, PostsFactory, $rootScope, $location) {
+
+ //       $scope.allVehicles = [];
+          
+          //get all vehicles
+ //        VehicleFactory.get().success( function (data) {
+ //         $scope.allVehicles = data.results;
+         
+ //       });
+        
+         //add a vehicle
+ //       $scope.addVehicle = function (v) {
+ //         VehicleFactory.add(v);
+
+ //       }, 
+    
+ //       $rootScope.$on('vehicle:added', function () {
+  //        $location.path('/');
+  //      });
+ //     }
+ //   ]);
+
+//}());
--- app/scripts/vehicle/vehicle.factory.js
+++ app/scripts/vehicle/vehicle.factory.js
@@ -1,39 +1,85 @@
-;(function (){
-  
+(function() {
+
   angular.module('Vehicle')
 
-  .factory('VehicleFactory', ['$http', 'PARSE', '$location', '$rootScope',
-    function ($http, PARSE, $location, $rootScope) {
+  .factory('VehicleFactory', [ '$http', 'PARSE', '$location', '$rootScope',
+    function($http, PARSE, $location, $rootScope){
+
+      // Getting a list of shows
+      var getAllVehicles = function(){
+      return  $http.get(PARSE.URL + 'classes/Shows', PARSE.CONFIG)
+      .success(function(){
+        $rootScope.$broadcast('allVehicles: list');
+      });
+    };
+
+
+      var addVehicle = function(obj){
+        $http.post(PARSE.URL + 'classes/Shows', obj, PARSE.CONFIG)
+        .success(function(){
+          $rootScope.$broadcast('show :add');
+
+        });
+      };
+
+
+
+
+      return {
+
+        get : getAllVehicles,
+        add: addVehicle
+      };
+    }
+
+  ]);
+
+}());
+
+
+
+
+
+
+
+
+
+//;(function (){
+
+//  'use strict'
+  
+ // angular.module('Vehicle')
+
+ // .factory('VehicleFactory', ['$http', 'PARSE', '$location', '$rootScope',
+ //   function ($http, PARSE, $location, $rootScope,) {
 
      
-
-
       //get a list of vehicles//
-       var getAllShows = function(){
-          return  $http.get(PARSE.URL + 'classes/Vehicle', PARSE.CONFIG)
-            .success(function(){
-            $rootScope.$broadcast('allVehicles: list');
-          });
-        };
+ //      var getAllVehicles = function(){
+ //         return  $http.get(PARSE.URL + 'classes/Vehicle', PARSE.CONFIG)
+ //           .success(function(){
+            //$rootScope.$broadcast('allVehicles: list');
+ //         });
+ //       };
 
       //add vehicle//
-      var addSingleVehicle = function (obj) {
-        $http.post(PARSE.URL + 'classes/Vehicle', obj, PARSE.CONFIG)
-          .success( function () {
-            $rootScope.$broadcast('vehicle:added');
-          });
-      };
+ //     var addVehicle = function (obj) {
+ //       $http.post(PARSE.URL + 'classes/Vehicle', obj, PARSE.CONFIG)
+  //        .success( function () {
+ //           $rootScope.$broadcast('vehicle :add');
+  //        });
+ //     };
 
      
       
  
-      return {
-        get : getAllVehicles,
-        add : addSingleVehicle
+//      return {
+//        get : getAllVehicles,
+//        add : addVehicle
         
-      }
-
+ //     }
+//
     }
-  ]);
+//  ]);
 
-}());
+//}());
